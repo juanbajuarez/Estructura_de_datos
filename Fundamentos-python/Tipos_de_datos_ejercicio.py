7b82b93c3985904f146ccfbed33734afe49ea9fb
Juan-Laptop <juanb8935@gmail.com>
2024-10-14 23:50:09 -0600

    Modificaciones finales al uso de variables

---
+++ Fundamentos-python/Tipos_de_datos_ejercicio.py
@@ -0,0 +1,33 @@
+'''
+Juan Bautista Juarez
+7 de septiembre de 2024.
+Descripción:
+Ejercicios para comprender y ejempleficar los datos en Python.
+'''
+
+Dia="lunes"
+Pasaje=21
+Comida=135.5
+Otros_Gastos=34.5
+Presupuesto=True
+
+print("Dia: ",Dia)
+print("Pasaje: ",Pasaje)
+print("Comida: ",Comida)
+print("Otros gastos: ",Otros_Gastos)
+print("¿Fue mayor a mi presupuesto?",Presupuesto)
+
+
+
+Dia="martes"
+Pasaje=12
+Comida=45.5
+Otros_Gastos=4.5
+Presupuesto=False
+
+
+print("Dia: ",Dia)
+print("Pasaje: ",Pasaje)
+print("Comida: ",Comida)
+print("Otros gastos: ",Otros_Gastos)
+print("¿Fue mayor a mi presupuesto?",Presupuesto)
