141fbc1fa85c150a2579d1cf11f1824e2c3af759
SALADECOMPUTO <juanb8935@gmail.com>
2024-10-15 21:53:22 -0600

    desde pc de sala de computo

---
+++ Operadores-en-Python/Operadores-asignacion.py
@@ -0,0 +1,39 @@
+# Juan bautista Juarez
+# 15  de octubre  de 2024
+# Operadores de asignacion en Python
+#asignacion multiple
+numero1,numero2=5,10
+numero3,numero4=9.14,"chuy"
+
+print(numero1)
+print(numero2)
+print(numero3)
+print(numero4)
+
+numero5,numero6=numero1+numero2,numero1-numero2
+
+print(numero5)
+print(numero6)
+
+#asignacion encadenada
+numero7=numero8=numero9=10
+
+print(numero7)
+print(numero8)
+print(numero9)
+
+#intercambio de variables
+
+numero10,numero11="albert","geto"
+
+print(numero10)
+print(numero11)
+numero11,numero10=numero10,numero11
+
+print(numero10)
+print(numero11)
+
+
+nombre,apellido=input("nombre: "),input("apellido: ")
+print(nombre)
+print(apellido)
---
+++ Operadores-en-Python/Operadores_aritmeticos.py
@@ -0,0 +1,23 @@
+# Juan bautista Juarez
+# 15  de octubre  de 2024
+# Operadores aritmeticos en Python
+
+
+numero1=input("Ingrese el primer numero: ")
+numero2=input("Ingrese el segundo numero: ")
+numero1=int(numero1)
+numero2=int(numero2)
+suma=numero1+numero2
+resta=numero1-numero2
+multiplicacion=numero1*numero2
+division=numero1/numero2
+modulo=numero1/numero2
+doble=numero1//numero2
+doblemultiplicacion=numero1**numero2
+print("Suma:",suma)
+print("Resta:",resta)
+print("Multiplicacaion:",multiplicacion)
+print("Division",division)
+print("Modulo:",modulo)
+print("Doble:",doble)
+print("Doblemultiplicaion:",doblemultiplicacion)
