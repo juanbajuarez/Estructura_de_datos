b5d21d3fa7d1dbb9475f7c54ea498dc49ebf19c0
SALADECOMPUTO <juanb8935@gmail.com>
2024-10-29 16:04:16 -0600

    desde pc de sala de computo 29/10/24

---
+++ Ciclos/Ciclos_ejercicio3_calculadora.py
@@ -0,0 +1,60 @@
+# Juan bautista Juarez
+# 29  de octubre  de 2024
+#Ciclo while en Python
+#Calculadora en Python
+
+print("*** Calculadora Básica ***")
+
+opcion=10
+while opcion!=0:
+    print()
+    print("Menú de operaciones")
+    print("[1]Suma")
+    print("[2]Resta")
+    print("[3]Multiplicación")
+    print("[4]División")
+    print("[5]División entera")
+    print("[6]Exponenciación")
+    print("[0]Salir")
+    opcion=int(input("Opción:"))
+    if opcion==1:
+        print("Suma")
+        numero1=float(input("Ingrese el primer numero: "))
+        numero2 = float(input("Ingrese el segundo numero: "))
+        suma=numero1+numero2
+        print(f"La suma es: {suma:.2f}")
+    elif opcion==2:
+        print("Resta")
+        numero1=float(input("Ingrese el primer numero: "))
+        numero2 = float(input("Ingrese el segundo numero: "))
+        resta=numero1-numero2
+        print(f"La resta es: {resta:.2f}")
+    elif opcion==3:
+        print("Multiplicación")
+        numero1=float(input("Ingrese el primer numero: "))
+        numero2 = float(input("Ingrese el segundo numero: "))
+        producto= numero1 * numero2
+        print(f"El producto es: {producto:.2f}")
+    elif opcion==4:
+        print("División")
+        numero1=float(input("Ingrese el primer numero: "))
+        numero2 = float(input("Ingrese el segundo numero: "))
+        division = numero1 / numero2
+        print(f"División es: {division:.2f}")
+    elif opcion==5:
+        print("División entera")
+        numero1=float(input("Ingrese el primer numero: "))
+        numero2 = float(input("Ingrese el segundo numero: "))
+        divisionE = numero1 // numero2
+        print(f"La división entera es: {divisionE}")
+    elif opcion==6:
+        print("Exponenciación")
+        numero1=float(input("Ingrese el primer numero: "))
+        numero2 = float(input("Ingrese el segundo numero: "))
+        exponenciacion = numero1 ** numero2
+        print(f"La exponenciación es: {exponenciacion:.2f}")
+    elif opcion==0:
+        print("Programa terminado")
+        opcion =0
+    else:
+        print("Opción inválida")
---
+++ Ciclos/Ciclos_ejercio4_cuenta_bancaria.py
@@ -0,0 +1,44 @@
+# Juan bautista Juarez
+# 29  de octubre  de 2024
+#Ciclo while en Python
+#Cuenta de banco
+
+print("PROGRAMA DE GESTION BANCARIA")
+print("*** Bienvenido al Banco azteca ***")
+
+saldo_inicial=1200
+
+opcion=10
+
+while opcion!=0:
+    print()
+    print("Menú de operaciones")
+    print("[1]Consultar saldo")
+    print("[2]Ingresar dinero")
+    print("[3]Retirar dinero")
+    print("[0]Salir")
+    opcion=int(input("Opción: "))
+    if opcion==1:
+        print("Consulta de saldo")
+        print(f"Su saldo disponibles es: {saldo_inicial:.2f}")
+
+    elif opcion==2:
+        print("Ingreso de dinero")
+        abono=float(input("Valor de la cantidad a ingresar: "))
+        saldo_inicial+=abono
+        print(f"Su saldo actual es {saldo_inicial:.2f}")
+
+    elif opcion == 3:
+        print("Retiro de dinero")
+        retiro = float(input("Valor de la cantidad a retirar: "))
+        if retiro<=saldo_inicial:
+            saldo_inicial -= retiro
+            print(f"Su saldo actual es {saldo_inicial:.2f}")
+        else:
+            print("Saldo insuficiente")
+
+    elif opcion == 0:
+        print("Programa terminado")
+        opcion = 0
+    else:
+        print("Opción inválida")
