7bc25800feb5146350d96390767342747aa45217
Juan-Laptop <juanb8935@gmail.com>
2024-10-28 23:00:05 -0600

    Programas comentados 28 de octubre

--- Operadores-en-Python/Operadores_logicos.py
+++ Operadores-en-Python/Operadores_logicos.py
@@ -1,6 +1,6 @@
-# Juan Bautista Juarez
-# 16  de Octubre  de 2024
-# Operadores logicos en Python
+# Juan Bautista Juárez.
+# 28 de octubre de 2024.
+# Operadores lógicos en Python.
 
 """
 Los operadores lógicos permiten combinar expresiones booleanas (verdadero o falso) para crear condiciones más complejas.
@@ -8,16 +8,22 @@ Estos operadores nos permiten realizar operaciones lógicas como "y", "o" y "no"
 decisiones más sofisticadas dentro de nuestros programas.
 """
 
-# Se solicita por consola que se ingresen dos valores (Si/No) para covnertirlas a expresiones booleanas.
+# Se solicita por consola que se ingresen dos valores (Si/No) para convertirlos en expresiones booleanas.
 expresion1 = input("Ingresa un valor (Si/No): ")
 expresion2 = input("Ingresa otro valor (Si/No): ")
 
-# Las cadenas se convierten a expresiones booleanas (ver Fundamentos-Python -> Entrada_conversiones.py).
+# Las cadenas se convierten a expresiones booleanas.
 expresion1 = expresion1.lower() == "si"
 expresion2 = expresion2.lower() == "si"
 
 # Se imprimen mensajes sobre operaciones lógicas.
-print(f"¿Ambas expresiones fueron 'si'? {expresion1 and expresion2}")
-print(f"¿Alguna expresión fue 'si'? {expresion1 or expresion2}")
+
+# Imprime "True" si ambas expresiones son verdaderas.
+print(f"¿Ambas expresiones fueron 'sí'? {expresion1 and expresion2}")
+
+# Imprime "True" si al menos una de las expresiones es verdadera.
+print(f"¿Alguna expresión fue 'sí'? {expresion1 or expresion2}")
+
+# Imprime lo contrario al valor lógico de la expresión dada.
 print(f"Lo contrario de la primera expresión es: {not expresion1}")
-print(f"Lo contrario de la segundo expresión es: {not expresion2}")
+print(f"Lo contrario de la segunda expresión es: {not expresion2}")
