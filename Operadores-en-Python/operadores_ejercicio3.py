839ab458fc73016f7c58305a62927a832ec53139
Juan-Laptop <juanb8935@gmail.com>
2024-10-28 22:08:07 -0600

    Programas comentados 28 de octubre

--- Operadores-en-Python/operadores_ejercicio3.py
+++ Operadores-en-Python/operadores_ejercicio3.py
@@ -1,9 +1,17 @@
-# Juan Bautista Juarez
-# 17  de Octubre  de 2024
-# Ejercicio número 3 de operadores en Python. Este programa indicará a el usuario si el
-# numero ingresado se encuentra en el rango de 10 a 15.
+# Juan Bautista Juárez.
+# 17 de octubre de 2024.
+# Ejercicio número 3 de operadores en Python. Este programa validará el usuario
+# y la contraseña ingresados por el usuario.
 
+# Declaración de cadenas como constantes.
+usuario_principal = "JuanElIng"  # Declaración del usuario.
+contraseña_principal = "2024-2"  # Declaración de la contraseña.
 
-numero=float(input("Ingrese un numero:"))
-print(f"El {numero} esta entre 10 y 15: {10<=numero and numero<=15}") #Sera True cuuando cumpla
-# las dos condiciones
+usuario = input("Ingrese su usuario: ")  # Solicita la entrada por teclado del usuario.
+contraseña = input("Ingrese su contraseña: ")  # Solicita la entrada por teclado de la contraseña.
+
+# Validación de que ambas entradas sean correctas.
+validacion = (usuario == usuario_principal) and (contraseña == contraseña_principal)
+
+# Imprime el resultado como un valor booleano.
+print("¿El acceso es correcto?", validacion)
