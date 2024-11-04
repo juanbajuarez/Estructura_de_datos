3174f57200fe7264dced6151f7aa1e54ca661b98
Juan-Laptop <juanb8935@gmail.com>
2024-11-03 18:49:33 -0600

    Examen 1P

--- Examen_Parcial_1/Ejercicio1_lic_informatica.py
+++ Examen_Parcial_1/Ejercicio1_lic_informatica.py
@@ -1,17 +1,17 @@
 """
-Programa 2 del examen.
+Programa 1 del examen.
 """
-print("**** Licenciatura en Informatica ***")
-inicio=1
-numero = int (input("Ingrese el numero final de la cuenta: "))
+print("**** Licenciatura en Informática ****")
+inicio = 1
+numero = int(input("Ingrese el número final de la cuenta: "))
 
-while inicio<=numero:
-    if inicio%5==0 and inicio%3==0:
-        print("Licenciatura en Informatica")
-    elif inicio%5==0:
-        print("Informatica",end=", ")
-    elif inicio%3==0:
-        print("Licenciatura",end=", ")
+while inicio <= numero:
+    if inicio % 5 == 0 and inicio % 3 == 0:
+        print("Licenciatura en Informática")
+    elif inicio % 5 == 0:
+        print("Informática", end=", ")
+    elif inicio % 3 == 0:
+        print("Licenciatura", end=", ")
     else:
-        print(f"{inicio}",end=", ")
-    inicio+=1
+        print(f"{inicio}", end=", ")
+    inicio += 1
