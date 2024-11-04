da92ef379bd5f69f1057bbae6c58d989f545ffe1
Juan-Laptop <juanb8935@gmail.com>
2024-11-03 18:55:48 -0600

    Examen 1P

--- Examen_Parcial_1/Ejercicio2_area_perimetro.py
+++ Examen_Parcial_1/Ejercicio2_area_perimetro.py
@@ -1,42 +1,52 @@
+"""
+Programa 2 del examen.
+"""
 import math
-print("*** Programa que calcula area y perimetro ***")
+print("*** Programa que calcula área y perímetro ***")
 
-opcion=10
+opcion = 10
 
-while opcion!=0:
+while opcion != 0:
     print()
     print("Menú de operaciones")
-    print("[1]Área de un rectángulo")
-    print("[2]Perimetro de un rectángulo")
-    print("[3]Área de un círculo")
-    print("[4]Perimetro de un círculo")
-    print("[0]Salir")
-    opcion=int(input("Opción:"))
-    if opcion==1:
+    print("[1] Área de un rectángulo")
+    print("[2] Perímetro de un rectángulo")
+    print("[3] Área de un círculo")
+    print("[4] Perímetro de un círculo")
+    print("[0] Salir")
+    opcion = int(input("Opción: "))
+
+    if opcion == 1:
         print("Área de un rectángulo")
-        numero1=float(input("Ingrese la base del rectangulo: "))
-        numero2 = float(input("Ingrese la altura del rectangulo: "))
-        area_rectangulo=numero1*numero2
-        print(f"EL área del rectangulo es: {area_rectangulo:.3f}")
-    elif opcion==2:
-        print("Perimetro de un rectángulo")
-        numero1 = float(input("Ingrese la base del rectangulo: "))
-        numero2 = float(input("Ingrese la altura del rectangulo: "))
-        perimetro_rectangulo=2*numero1+2*numero2
-        print(f"El perimetro del rectangulo: {perimetro_rectangulo:.3f}")
-    elif opcion==3:
+        base = float(input("Ingrese la base del rectángulo: "))
+        altura = float(input("Ingrese la altura del rectángulo: "))
+        area_rectangulo = base * altura
+        print(f"El área del rectángulo es: {area_rectangulo:.3f}")
+
+    elif opcion == 2:
+        print("Perímetro de un rectángulo")
+        base = float(input("Ingrese la base del rectángulo: "))
+        altura = float(input("Ingrese la altura del rectángulo: "))
+        perimetro_rectangulo = 2 * (base + altura)
+        print(f"El perímetro del rectángulo es: {perimetro_rectangulo:.3f}")
+
+    elif opcion == 3:
         print("Área de un círculo")
-        numero1=float(input("Ingrese el radio del circulo: "))
-        area_circulo= math.pi*numero1**2
-        print(f"El área de un circulo es: {area_circulo:.3f}")
-    elif opcion==4:
-        print("Perimetro de un círculo")
-        numero1 = float(input("Ingrese el radio del circulo: "))
-        perimetro_circulo = math.pi * numero1 * 2
-        print(f"El perimetro de un circulo es: {perimetro_circulo:.3f}")
-    elif opcion==0:
+        radio = float(input("Ingrese el radio del círculo: "))
+        area_circulo = math.pi * radio ** 2
+        print(f"El área del círculo es: {area_circulo:.3f}")
+
+    elif opcion == 4:
+        print("Perímetro de un círculo")
+        radio = float(input("Ingrese el radio del círculo: "))
+        perimetro_circulo = 2 * math.pi * radio
+        print(f"El perímetro del círculo es: {perimetro_circulo:.3f}")
+
+    elif opcion == 0:
         print("Programa terminado")
-        opcion =0
+
     else:
         print("Opción inválida")
+
     print("*******************************")
+
