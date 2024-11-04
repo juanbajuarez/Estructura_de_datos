4f6f18aa944849fa4b864bb0e77abd5d61422398
Juan-Laptop <juanb8935@gmail.com>
2024-11-03 19:20:25 -0600

    Examen 1P

--- Examen_Parcial_1/Ejercicio4_juego_adivinador.py
+++ Examen_Parcial_1/Ejercicio4_juego_adivinador.py
@@ -1,24 +1,25 @@
 """
-Programa 1 del examen.
+Programa 4 del examen.
 """
+from random import randint
 
-from random import *
-numero=randint(0, 100)
-contador=1
-print(numero)
-while contador<=5:
-    intento=int(input("Ingrese un numero: "))
-    if intento>numero:
-        print("EL numero ingresado es mayor al numero buscado")
-        contador+=1
-    elif intento<numero:
-        print("EL numero ingresado es menor al numero buscado")
+print("*** Programa del juego del adivinador ***")
+numero = randint(0, 100)
+contador = 1
+
+while contador <= 5:
+    print(f"Número de intento: {contador}", end=" ")
+    intento = int(input("Ingrese un número entre 1 y 100: "))
+
+    if intento > numero:
+        print("El número ingresado es mayor al número buscado.")
         contador += 1
-    elif intento==numero:
-        print("*** Felicidades ***")
-        print("Adivinaste el numero")
-        contador =100
-    else:
-        print()
-    if contador==6:
-        print("*** Numero no adivinado ***")
+    elif intento < numero:
+        print("El número ingresado es menor al número buscado.")
+        contador += 1
+    elif intento == numero:
+        print(f"*** ¡Felicidades! Adivinaste en {contador} intentos. ***")
+        break
+
+    if contador == 6:
+        print(f"*** Perdiste. El número era: {numero}. ***")
