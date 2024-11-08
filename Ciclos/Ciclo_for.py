74a41e0a7757c9ff94f08098b007b892692a8a82
SALADECOMPUTO <juanb8935@gmail.com>
2024-11-07 22:10:40 -0600

    ciclo for desde sc2 falta comentar

--- Ciclos/Ciclo_for.py
+++ Ciclos/Ciclo_for.py
@@ -4,20 +4,24 @@
 
 
 """
-sintaxis
+sintaxis del ciclo for
 for variable in secuencias:
 
+
+Programa que imprime una lista de nombres
 """
 cadena_de_usuario=input("Ingrese una cadena:")
 contador=0
+
+#Ciclo for que cuenta el numero de caracteres de una cadena ingresada y lo imprime
 for caracter in cadena_de_usuario:
     contador+=1
     print(f"{caracter}",end=" - ")
 print()
 print(contador)
-
+#declaracion de una lista de nmbres
 alumnos=["Rosalinda","Juan","Lourdes","Tania","Bryan","Rebeca","Jennifer","Hector","Galilea","Patricia","Jesus","Addi"]
-
+#Impreme hola y el nombre del alumno
 for alumno in alumnos:
     print(f"Hola {alumno}")
 
