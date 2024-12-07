338222e1320a682c6e31df0c8ac3aaddf4f3aba9
Juan-Laptop <juanb8935@gmail.com>
2024-12-07 13:54:31 -0600

    Lap

--- Funciones y Colecciones/Diccionarios_ej1_piedra_papel_tijera.py
+++ Funciones y Colecciones/Diccionarios_ej1_piedra_papel_tijera.py
@@ -1,25 +1,65 @@
 # Autor: Juan Bautista Juárez
 # Fecha: Diciembre de 2024
-# Descripción: Introduccion a diccionarios en Python.
+# Descripción: Juego de piedra, papel o tijeras utilizando
+# diccionarios en Python.
 
 """
 Por clave-valor
 Ejemplos de usos
 nombre_diciionarios={'key':"valor",'key2':"valor2"}
 """
-from Ciclos.Ciclos_ej4_cuenta_bancaria import opcion
+from numpy.random import choice
 
-PIEDRA="Piedra"
-PAPEL="Papel"
-TIJERAS="Tijeras"
-JUGADOR="Gana el juagador"
-EMPATE="Empate"
-CPU="Gana la cpu"
 
-#Función del menu
+# Función que asigna los valores del jugador y la CPU.
+def jugador(opcion):
+    opciones = ["PIEDRA", "PAPEL", "TIJERAS"]
+    eleccion_usuario = opciones[opcion - 1]
+    eleccion_cpu = choice(["PIEDRA", "PAPEL", "TIJERAS"])
+    return eleccion_usuario, eleccion_cpu
+
+
+# Función del menu
 def menu():
     print("1) PIEDRA")
     print("2) PAPEL")
-    print("3) TIJERA")
-    opcion=int(input("Ingrese su opcion: "))
+    print("3) TIJERAS")
+    print("0) SALIR")
+    opcion = int(input("Ingrese su opcion: "))
     return opcion
+
+
+# Código a nivel de módulo.
+jugador_v = cpu = empate = 0
+opcion = None
+while opcion != 0:
+    print("***  Juego de piedra, papel o tijeras  ***")
+    opcion = menu()
+    if opcion > 3 or opcion < 0:
+        print("Opcion invalida")
+    elif opcion == 0:
+        print("Programa terminado")
+        break
+    else:
+        diccionario = jugador(opcion)
+        print()
+        piedra_papel_tijera = {('PIEDRA', 'TIJERAS'): "JUGADOR", ('PIEDRA', 'PAPEL'): "CPU",
+                               ('TIJERAS', 'PAPEL'): "JUGADOR", ('TIJERAS', 'PIEDRA'): "CPU",
+                               ('PAPEL', 'PIEDRA'): "JUGADOR", ('PAPEL', 'TIJERAS'): "CPU", }
+
+        # Lógica para determinar al ganador.
+        resultado = piedra_papel_tijera.get(diccionario)
+        if resultado == "JUGADOR":
+            print(f"Jugador: {diccionario[0]}. CPU: {diccionario[1]}. "
+                  f"El resultado es: Gana del Jugador.")
+            jugador_v += 1
+        elif resultado == "CPU":
+            print(f"Jugador: {diccionario[0]}. CPU: {diccionario[1]}. "
+                  f"El resultado es: Gana la CPU.")
+            cpu += 1
+        else:
+            print(f"Jugador: {diccionario[0]}. CPU: {diccionario[1]}. "
+                  f"El resultado es: Empate.")
+            empate += 1
+
+        print(f"Victorias del jugador: {jugador_v}. Victorias de la CPU: {cpu}. Empates: {empate}.")
